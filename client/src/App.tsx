f05f718451c704ef4c5be597ce31d3b1b784c024
Sayan Mallick <snyxmk@gmail.com>
2021-02-09 21:01:14 +0530

    Adding Tailwind CSS and Prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,17 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <div className="min-h-screen flex flex-col mx-auto max-w-3xl px-4 md:px-0">
+        <header className="py-40 sm:py-20 ">
+          <h1 className="text-4xl sm:text-6xl font-black text-center sm:text-left text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-green-500">
+            Bloop ...
+          </h1>
+        </header>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
