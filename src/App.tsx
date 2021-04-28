a418960feeb821d52ab0b371851a39ffc3064091
Sayan Mallick <snyxmk@gmail.com>
2021-04-28 23:04:48 +0530

    Pages Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React, { Fragment } from "react";
 import { Helmet } from "react-helmet-async";
-import { useHelloQuery } from "./generated-types";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/index";
 import About from "./pages/about";
 
 export default function App() {
-  const { loading, error, data } = useHelloQuery();
-
-  console.log(data);
-
   return (
     <Fragment>
       <Helmet>
@@ -17,10 +12,6 @@ export default function App() {
         <title>Bloop 🐱 </title>
         <link rel="icon" type="image/svg+xml" href="static/pawprint.svg" />
       </Helmet>
-      <div>
-        <p>{data?.hello.text}</p>
-        <p>{data?.hello.time}</p>
-      </div>
       <div>
         <nav>
           <ul>
@@ -32,14 +23,16 @@ export default function App() {
             </li>
           </ul>
         </nav>
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <div className="flex justify-center bg-red-300">
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Fragment>
   );
